00d8d7ee3d4fa92c74fce199d158569c0f2dada7
ayaanzaheer <ayaanzaheer@gmail.com>
2026-02-07 19:57:16 -0800

    Finished LinkedList lab implementation and driver tests

--- lab04/LinkedList.tpp
+++ lab04/LinkedList.tpp
@@ -10,16 +10,49 @@ LinkedList<T>::~LinkedList() {
 template <typename T>
 void LinkedList<T>::append(const T& elem) {
     // TODO
+    Node* newNode = new Node(elem);
+    if(head == nullptr){        // if empty attatch the node to head
+        head = newNode;
+    }
+    else{                       // if not then traverse through it and attatch when there is null
+        Node* cur = head;
+        while(cur->next != nullptr){            // use current to traverse over and over
+            cur = cur->next;
+        }
+        cur->next = newNode;        // making the ptr point to new node;
+    }
+    this->length++;
 }
 
 template <typename T>
 void LinkedList<T>::clear() {
     // TODO
+    Node* cur = head;       // make current at the same place as head
+
+    while(cur != nullptr){      // go through list until null; throughout list save current node to delete it and then move to next one and delete previous
+        Node* temp = cur;
+        cur = cur->next;
+        delete temp;
+    }
+    head = nullptr;     // set head to null and make length 0
+    this->length = 0;
 }
 
 template <typename T>
 T LinkedList<T>::getElement(int position) const {
     // TODO
+    Node* cur = head;           // same stuff as before, make the current where the head is and also set index to 0 for later
+    int index = 0;
+
+    while (cur != nullptr){
+        if(index == position){
+            return cur->value;          // if element found then return it
+        }
+        cur = cur->next;                // if not found then move forward and increase index
+        index++;
+    }
+
+    throw string("invalid position");        // throw invalid if not found becasue if found it returns before this
 }
 
 template <typename T>
@@ -35,6 +68,18 @@ bool LinkedList<T>::isEmpty() const {
 template <typename T>
 void LinkedList<T>::replace(int position, const T& elem) {
     // TODO
+    Node* cur = head;           // same stuff as before, make the current where the head is and also set index to 0 for later
+    int index = 0;
+
+    while (cur != nullptr){
+        if(index == position){
+            cur->value = elem;
+            return;         // change value then return if found what to change
+        }
+        cur = cur->next;                // if not found then move forward and increase index
+        index++;
+    }
+    throw string("invalid position");        // throw invalid if not found becasue if found it returns before this
 }
 
 template <typename T>
--- lab04/driver.cpp
+++ lab04/driver.cpp
@@ -1,1 +1,56 @@
 // TO DO: Implement the driver main() function to test your code.
+
+#include <iostream>
+#include "LinkedList.hpp"
+#include <string>
+using namespace std;
+
+int main(){
+
+    LinkedList<int> list1;       // make list called list
+
+    cout << "is list empty? (1 if true and 0 if false): " << list1.isEmpty() << endl;    // check if empty to show it
+    cout << "length right now is: " << list1.getLength() << endl; // show lenght
+
+    list1.append(67);
+    list1.append(-67);               // append values; basically adding it to the list
+    list1.append(670);
+
+    cout << "list now is: " << list1 << endl;                    // show list and legnth now
+    cout << "length now is: " << list1.getLength() << endl;
+
+    cout << "Element at index 2: " << list1.getElement(2) << endl; // show the value at index 2
+
+    list1.replace(2,99);
+    cout << "after replacing value the list is: " << list1 << endl;      // replace index 2 with 99
+
+    cout << "is list empty before clearing? (1 if true and 0 if false): " << list1.isEmpty() << endl;    // check if empty to show it
+    list1.clear();                                               // clear it
+    cout << "is list empty after clearing? (1 if true and 0 if false): " << list1.isEmpty() << endl;    // check if empty to show it
+
+
+//*******************doing a new one with list of strings for testing */
+
+    LinkedList<string> list2;       // make list called list
+
+    cout << "is list empty? (1 if true and 0 if false): " << list2.isEmpty() << endl;    // check if empty to show it
+    cout << "length right now is: " << list2.getLength() << endl; // show lenght
+
+    list2.append("lebron");
+    list2.append("jordan");               // append values; basically adding it to the list
+    list2.append("shaq");
+
+    cout << "list now is: " << list2 << endl;                    // show list and legnth now
+    cout << "length now is: " << list2.getLength() << endl;
+
+    cout << "Element at index 2: " << list2.getElement(2) << endl; // show the value at index 2
+
+    list2.replace(2,"bronny");
+    cout << "after replacing value the list is: " << list2 << endl;      // replace index 2 with bronnny
+
+    cout << "is list empty before clearing? (1 if true and 0 if false): " << list2.isEmpty() << endl;    // check if empty to show it
+    list2.clear();                                               // clear it
+    cout << "is list empty after clearing? (1 if true and 0 if false): " << list2.isEmpty() << endl;    // check if empty to show it
+
+    return 0;
+}
